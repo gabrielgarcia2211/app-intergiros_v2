0a1edea84b851ede633271f39dc2db06a989ec02
Danielcaos <danielcaos@ufps.edu.co>
2023-10-25 19:49:40 -0500

    styles for reset password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,26 +1,36 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container login">
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+<div class="container login">
+    <div class="text-center">
+        <h4><strong>¿Olvidaste tu contraseña? ¡No te preocupes!</strong></h4>
+    </div>
+    <hr>
+    <br>
+    <div class="text-center mt-5">
+        <h5>Ingresa tu correo electrónico para restablecer la contraseña</h5>
+    </div>
+    <form method="POST" action="{{ route('password.update') }}">
+        @csrf
 
-            <input type="hidden" name="token" value="{{ $token }}">
+        <input type="hidden" name="token" value="{{ $token }}">
 
-            <div class="row mb-3">
-                <div class="col-md-12">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                        name="email" value="{{ $email ?? old('email') }}" placeholder="Correo Electronico" required
-                        autocomplete="email" autofocus>
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
+        <div class="form-row mt-5">
+            <div class="form-group col-md-6">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                    value="{{ $email ?? old('email') }}" placeholder="Correo Electronico" required autocomplete="email"
+                    autofocus>
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
+            <div class="form-group col-md-6"></div>
+        </div>
 
-            <div class="row mb-3">
-                <div class="input-group" style="width: 97% !important">
+        <div class="form-row mt-1">
+            <div class="form-group col-md-6">
+                <div class="input-group">
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
                         name="password" placeholder="Contraseña nueva" required autocomplete="new-password">
                     <div class="input-group-append">
@@ -29,14 +39,13 @@
                         </span>
                     </div>
                     @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
                     @enderror
                 </div>
             </div>
-
-            <div class="row mb-3">
+            <div class="form-group col-md-6">
                 <div class="input-group" style="width: 97% !important">
                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
                         placeholder="Confirmar contraseña" required autocomplete="new-password">
@@ -47,26 +56,29 @@
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="text-center mt-5">
-                <div class="row">
-                    <div class="col-md-12 button">
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Reset Password') }}
-                        </button>
-                    </div>
+
+
+        <div class="text-center mt-5 button">
+            <div class="row">
+                <div class="col-md-12 button">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Reset Password') }}
+                    </button>
                 </div>
             </div>
-        </form>
+        </div>
+    </form>
 
 
-        <br>
-    </div>
-    <br><br><br><br><br>
-    <!-- FOOTER -->
-    @include('layouts.footer')
+    <br>
+</div>
+<br><br><br><br><br>
+<!-- FOOTER -->
+@include('layouts.footer')
 @endsection
 
 @section('script')
-    <script src="{{ asset('js/recuperacion/index.js') }}"></script>
+<script src="{{ asset('js/recuperacion/index.js') }}"></script>
 @endsection
