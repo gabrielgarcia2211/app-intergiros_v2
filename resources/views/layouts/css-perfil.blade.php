7ce4d904215d24e9e39de89c2b4a128d25a36054
Gabriel Arturo Garcia <61440596+gabrielgarcia2211@users.noreply.github.com>
2024-05-21 19:47:57 -0500

    ajustes de versionamiento de js y css

--- resources/views/layouts/css-perfil.blade.php
+++ resources/views/layouts/css-perfil.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
-    <link href="{{ asset('css/home-auth.css') }}?v={{ uniqid() }}" rel="stylesheet" />
+    <link href="{{ asset('css/perfil.css') }}?v={{ uniqid() }}" rel="stylesheet" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
